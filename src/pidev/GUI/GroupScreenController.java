5f63d81ea5c7b785fee7fab5725c8fcbc1107513
MohamedTestouri <mohamed..testouri@esprit.tn>
2020-02-18 16:34:28 +0100

    Group added miss the updates

--- src/pidev/GUI/GroupScreenController.java
+++ src/pidev/GUI/GroupScreenController.java
@@ -5,15 +5,33 @@
  */
 package pidev.GUI;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 import pidev.Entite.Groups;
+import pidev.Entite.Users;
+import pidev.Service.GroupService;
+import pidev.Service.UserService;
 
 /**
  * FXML Controller class
@@ -22,6 +40,8 @@ import pidev.Entite.Groups;
  */
 public class GroupScreenController implements Initializable {
 
+    @FXML
+    private TableView<Groups> Table;
     @FXML
     private TableColumn<Groups, Integer> IDGroup;
     @FXML
@@ -42,13 +62,62 @@ public class GroupScreenController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    ObservableList<Groups> listGroups = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        // 1. fill in the table view
+        GroupService GS = new GroupService();
+        try {
+            listGroups.addAll(GS.readAll());
+        } catch (SQLException e) {
+            Logger.getLogger(UserScreenController.class.getName()).log(Level.SEVERE, null, e);
+        }
+        IDGroup.setCellValueFactory(new PropertyValueFactory<>("idGroup"));
+        NameGroup.setCellValueFactory(new PropertyValueFactory<>("nameGroup"));
+        TypeGroup.setCellValueFactory(new PropertyValueFactory<>("typeGroup"));
+        Table.setItems(listGroups);
+
+        // 2. search bar 
+        FilteredList<Groups> filteredData = new FilteredList<>(listGroups, lu -> true);
+        SearchTermTextFiled.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate((Groups group) -> {
+                // 2.1. If filter text is empty, display all persons.
+
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                // 2.2. Compare id name and type of every group with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (group.getNameGroup().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // 2.2.1. Filter matches  name.
+                } else if (group.getTypeGroup().toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // 2.2.2. Filter matches type.
+                } else if (String.valueOf(group.getIdGroup()).contains(lowerCaseFilter)) {
+                    return true; // 2.2.3. Filter matches id
+                } else {
+                    return false; // Does not match.
+                }
+            });
+        });
+        //3. sorted list
+        // 3.1. Wrap the FilteredList in a SortedList. 
+        SortedList<Groups> sortedData = new SortedList<>(filteredData);
+
+        // 3.2. Bind the SortedList comparator to the TableView comparator.
+        // 	  Otherwise, sorting the TableView would have no effect.
+        sortedData.comparatorProperty().bind(Table.comparatorProperty());
+
+        // 3.3. Add sorted (and filtered) data to the table.
+        Table.setItems(sortedData);
+    }
 
     @FXML
-    void deleteGroup(ActionEvent event) {
+    void deleteGroup(ActionEvent event) throws SQLException {
+        UserService US = new UserService();
+        US.delete(IDGroup.getCellData(Table.getSelectionModel().getSelectedIndex()));
+        JOptionPane.showMessageDialog(null, "Group Deleted");
+        Table.refresh();
     }
 
     @FXML
@@ -56,7 +125,13 @@ public class GroupScreenController implements Initializable {
     }
 
     @FXML
-    void addGroup(ActionEvent event) {
+    void addGroup(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddGroupScreen.fxml"));
+        Parent root1 = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Hunt Kingdom | Admin | Add User");
+        stage.setScene(new Scene(root1));
+        stage.show();
     }
-    
+
 }
